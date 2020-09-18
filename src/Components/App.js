9a61e8ae255a44c1a9b0f148867ad0e91b448355
Darshan <dj30c.1@gmail.com>
2020-09-18 16:54:20 +0530

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Tripoly</h1>
+      <h2>Work in progress</h2>
+    </div>
+  );
+}
