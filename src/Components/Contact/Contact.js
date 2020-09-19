aad9b0bf8c80c42eda46aace7a22bd8872b93a57
Darshan <dj30c.1@gmail.com>
2020-09-19 17:21:00 +0530

    Add Social links

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,7 +2,40 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div className="Contact">Contact</div>;
+  return (
+    <div className="Contact">
+      <button>Contact Us</button>
+      <section className="social">
+        <header>Keep in touch</header>
+        <div className="social-links">
+          <a
+            id="facebook"
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a
+            id="insta"
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            id="youtube"
+            href="https://www.youtube.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;
