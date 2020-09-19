37c96c735dc51ecfe8e145ff7b0272101d7b5cb1
Darshan <dj30c.1@gmail.com>
2020-09-19 18:49:00 +0530

    Complete features

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  const [visible, setVisibility] = useState(false);
+  const [msg, setMsgVisible] = useState(false);
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    const form = document.querySelector("form.content");
+    let complete = true;
+    const firstName = form.querySelector("#first-name").value;
+    const LastName = form.querySelector("#last-name").value;
+    const email = form.querySelector("#email").value;
+    const bio = form.querySelector("#bio").value;
+    complete = complete && firstName && LastName && email && bio;
+    setVisibility(false);
+    complete ? setMsgVisible("Thank you!") : setMsgVisible("Form Incomplete!");
+  };
+
+  const popUp = (
+    <div className="pop-up">
+      <form className="content" onSubmit={(e) => formHandler(e)}>
+        <input type="text" id="first-name" placeholder="First name" />
+        <input type="text" id="last-name" placeholder="Last name" />
+        <input type="email" id="email" placeholder="Email" />
+        <input type="textarea" id="bio" placeholder="Bio" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+
+  const popOver = (
+    <div className="pop-up">
+      <div className="content">
+        <div>{msg}</div>
+        <button onClick={() => setMsgVisible(false)}>Ok</button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="Contact">
-      <button>Contact Us</button>
+      {msg ? popOver : null}
+      {visible ? popUp : null}
+      <button onClick={() => setVisibility(true)}>Contact Us</button>
       <section className="social">
         <header>Keep in touch</header>
         <div className="social-links">
